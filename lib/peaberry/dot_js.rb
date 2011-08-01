5fea25332869cb0a3258448887f81e627e720e33
OZAWA Sakuro <sakuro@2238club.org>
2011-08-01 15:14:01 +0900

    Use Rack::Utils::HeaderHash.

--- lib/peaberry/dot_js.rb
+++ lib/peaberry/dot_js.rb
@@ -14,17 +14,16 @@ module Peaberry
     end
 
     def call(env)
+      headers = Rack::Utils::HeaderHash.new
       request = Rack::Request.new(env)
       files = []
       files << @sprockets.find_asset('default.js')
       files << @sprockets.find_asset(request.path_info.sub(/^\//, ''))
 
       body = "// dotjs is working! //\n" + files.compact.map(&:to_s).join
-      headers = {
-        'Content-Length' => body.bytesize.to_s,
-        'Content-Type' => 'text/javascript',
-        'Access-Control-Allow-Origin' => '*'
-      }
+      headers['Content-Length'] = body.bytesize.to_s
+      headers['Content-Type'] = 'text/javascript'
+      headers['Access-Control-Allow-Origin'] = '*'
       [ 200, headers, body.lines ]
     end
   end
