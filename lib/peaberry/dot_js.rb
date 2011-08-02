f3e519622ead3a83fb5fc3b61a9b1487c960f934
OZAWA Sakuro <sakuro@2238club.org>
2011-08-02 16:36:36 +0900

    Set Access-Control-Allow-Origin after testing Origin.

--- lib/peaberry/dot_js.rb
+++ lib/peaberry/dot_js.rb
@@ -16,17 +16,26 @@ module Peaberry
     def call(env)
       headers = Rack::Utils::HeaderHash.new
       request = Rack::Request.new(env)
+
+      path = request.path_info
       files = []
       files << @sprockets.find_asset('default.js')
-      files << @sprockets.find_asset(request.path_info.sub(/^\//, ''))
+      files << @sprockets.find_asset(path.sub(/^\//, ''))
 
       body = "// dotjs is working! //\n" + files.compact.map(&:to_s).join
+
       headers['Content-Length'] = body.bytesize.to_s
       headers['Content-Type'] = 'text/javascript'
-      headers['Access-Control-Allow-Origin'] = '*'
+      origin = env['HTTP_ORIGIN']
+      search = path.gsub('/','').gsub(/\.js$/,'') + '$'
+      if origin && origin.length == 1 && path.length != 1 && origin[0].match(search)
+        headers['Access-Control-Allow-Origin'] = origin[0]
+      end
+
       [ Rack::Utils.status_code(:ok), headers, body.lines ]
     rescue => e
       [ Rack::Utils.status_code(:internal_server_error), headers, [e.message]]
     end
   end
+
 end
