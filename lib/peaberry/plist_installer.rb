2eac15a39f2f4d6fb856d07af2c090f0c94a77f4
OZAWA Sakuro <sakuro@2238club.org>
2011-08-31 14:13:22 +0900

    Ensure the destination directory exists.

--- lib/peaberry/plist_installer.rb
+++ lib/peaberry/plist_installer.rb
@@ -1,12 +1,15 @@
 # -*- encoding: UTF-8 -*-
 
 require 'erb'
+require 'fileutils'
 
 module Peaberry
   class PlistInstaller
     PLIST_ERB_PATH = File.join(File.dirname(__FILE__), '../../com.github.peaberry.plist.erb')
     PLIST_PATH = File.expand_path('~/Library/LaunchAgents/com.github.peaberry.plist')
     def install
+      dir = File.dirname(PLIST_PATH)
+      FileUtils.mkdir_p(dir) unless FileTest.directory?(dir)
       open(PLIST_PATH, 'w') do |plist|
         ruby_bin_path =
           if RbConfig.respond_to?(:ruby)
